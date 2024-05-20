9f20c8c4e6304dad45e2c17dabf7db62d7d4c518
Arslan Kamchybekov <86550318+ArslanKamchybekov@users.noreply.github.com>
2024-05-19 20:19:30 -0500

    Add files via upload

---
+++ GCTest.java
@@ -0,0 +1,13 @@
+//import java.util.ArrayList;
+//
+//public class GCTest {
+//    private static ArrayList<Integer> list = new ArrayList<>();
+//    public static void main(String[] args) {
+////        for (int i = 0; i < 10000000; i++) {
+////            list.add((int)(Math.random() * 10));
+////        }
+////        for (int i = 0; i < 10000; i++) {
+////            list.remove(i);
+////        }
+//    }
+//}
---
+++ MemoryLeak.java
@@ -0,0 +1,12 @@
+//import java.util.ArrayList;
+//import java.util.Random;
+//
+//public class MemoryLeak {
+//    //private static ArrayList<Integer> list = new ArrayList<>();
+//    //private static Random random = new Random();
+//    public static void main(String[] args) {
+////        while(true){
+////            list.add(random.nextInt());//heap space
+////        }
+//    }
+//}
